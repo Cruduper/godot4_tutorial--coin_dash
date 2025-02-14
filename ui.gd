d1b896dea308cab997908f03995eb0bd86e8015a
Cruduper <briefcasemanx@gmail.com>
2025-02-14 02:00:54 -0800

    Create UI scene

---
+++ ui.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+signal start_game
+
+func update_score(value):
+	$MarginContainer/Score.text = str(value)
+	
+func update_timer(value):
+	$MarginContainer/Time.text = str(value)
+	
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$Timer.start()
+	
+func _on_timer_timeout():
+	$Message.hide()
